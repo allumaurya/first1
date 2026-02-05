89bd36350ad3b98244c311fe99182eef38d9b023
allumaurya <129435412+allumaurya@users.noreply.github.com>
2026-02-05 22:35:49 +0530

    Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Port from './assets/Port'
 import Nav from './assets/Nav'
-<<<<<<< HEAD
 import Updiv from './assets/Updiv'
 import Updiv2 from './assets/Updiv2'
 import Navbar from './assets/Navbar'
@@ -15,16 +14,11 @@ import Course from './assets/course'
 import Registar from './Registar'
 import Port2 from './component/Port2'
 import Skill from './component/Skill'
-=======
-
-
->>>>>>> bc34d38b520a66d9401de3fca9a75ce83778140c
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<<<<<<< HEAD
          {/*  <Updiv/> */}
  <Nav/>
     <Port/>
@@ -38,13 +32,6 @@ function App() {
   <Course/>
   <Registar/>
      */}
-=======
-   
-    <Nav/>
-    
- 
-  <Port/> 
->>>>>>> bc34d38b520a66d9401de3fca9a75ce83778140c
      
     </>
   )
